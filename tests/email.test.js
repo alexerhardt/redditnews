e33bb10530a9a5a4dab7ba428ec3c591688bb22d
A <alex.erhardt.es@gmail.com>
2019-08-10 19:22:39 +0200

    add test skeleton

---
+++ tests/email.test.js
@@ -0,0 +1,17 @@
+describe('Email creation and sending', () => {
+  it('should notify users if the subreddit no longer exists', () => {
+    // TODO
+  });
+
+  it('should create as many emails as subreddits', () => {
+    // TODO
+  });
+
+  it('should send as many emails as subscriptions', () => {
+    // TODO
+  });
+
+  it('should form a valid email', () => {
+    // TODO
+  });
+});
---
+++ tests/server.test.js
@@ -0,0 +1,33 @@
+// TODO: Write function to populate DB
+
+describe('POST /subscribe', () => {
+  it('should create a new subscription', done => {
+    // TODO
+  });
+
+  it('should not create a subscription with invalid email', done => {
+    // TODO
+  });
+
+  it('should not create a subscription with invalid subreddit name', done => {
+    // TODO
+  });
+});
+
+describe('POST /unsubscribe', () => {
+  it('should unsubscribe from all', done => {
+    // TODO
+  });
+
+  it('should not unsubscribe from all with invalid token', done => {
+    // TODO
+  });
+
+  it('should unsubscribe from one', done => {
+    // TODO
+  });
+
+  it('should not unsubscribe from one with invalid token', done => {
+    // TODO
+  });
+});
